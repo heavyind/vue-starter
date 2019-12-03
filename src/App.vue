ceece57fc3279db3306772bfd6f7f5bb8e93b941
Chase Ries <hello@chaseries.com>
2019-12-03 10:08:22 -0500

    Beef up modal options

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       @afterEnter="preloadAfterEnter">
       <preloader v-if="!ready"></preloader>
       <div v-else>
-        <modal :closeDelay="500"></modal>
+        <modal transitionName="trans-modal" :closeDelay="500"></modal>
         <trans-fade-in-fade-out :delay="250" :initOnly="true">
           <header>
             <nav>
